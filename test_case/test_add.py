d99553d0ece70f02a9fccdeddef4bc38ea3d8519
SEtester <dengjiajie0109@gmail.com>
2018-12-02 00:01:32 +0800

    整理代码，文件名

--- test_case/test_add.py
+++ test_case/test_add.py
@@ -1,19 +1,17 @@
 # encoding:utf8
 
 import unittest
-import pytest
 
 
 class AddCase(unittest.TestCase):
 
-    @pytest.mark.bvt
+    def add_fun(self, a, b):
+        return a + b
+
     def test_add_1(self):
         '''加法冒烟测试'''
         ret = self.add_fun(1, 2)
         self.assertEqual(3, ret)
-
-    def add_fun(self, a, b):
-        return a + b
 
     def test_add_2(self):
         ret = self.add_fun(1, 2)
--- test_case/test_sub.py
+++ test_case/test_sub.py
@@ -1,19 +1,17 @@
 # encoding:utf8
 
 import unittest
-import pytest
 
 
 class SubCase(unittest.TestCase):
 
-    @pytest.mark.bvt
+    def add_fun(self, a, b):
+        return a - b
+
     def test_sub_1(self):
         '''减法冒烟测试'''
         ret = self.add_fun(1, 2)
         self.assertEqual(-1, ret)
-
-    def add_fun(self, a, b):
-        return a - b
 
     def test_sub_2(self):
         ret = self.add_fun(1, 2)
