39716c81c1851d5c49fb3ec688bed0f59435787b
SEtester <dengjiajie0109@gmail.com>
2018-12-04 20:53:16 +0800

    增加loadTestsFromNames例子

--- run_from_case_names.py
+++ run_from_case_names.py
@@ -8,7 +8,12 @@ dir_run_test_case = os.path.dirname(os.path.abspath(__file__))
 dir_test_case = dir_run_test_case + '/test_case'
 sys.path.insert(0, dir_test_case)
 
-cases = ['test_sub.SubCase.test_sub_1','test_add.AddCase.test_add_1']
+# 例一
+# cases = ['test_sub','test_add']
+# 例二
+cases = ['test_sub.SubCase','test_add.AddCase.test_add_1']
+# 例三
+# cases = ['test_sub.SubCase.test_sub_1','test_add.AddCase.test_add_1']
 suite = unittest.TestLoader().loadTestsFromNames(cases)
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(suite)
